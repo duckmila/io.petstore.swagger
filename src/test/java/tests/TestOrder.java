f9342a95f511ff55f5dd3f9ac2050affde2ad720
Ludmila Haluchenko <duckmila@gmail.com>
2020-02-12 23:32:26 +0500

    Added test file for order

---
+++ src/test/java/tests/TestOrder.java
@@ -0,0 +1,20 @@
+package tests;
+
+import models.Order;
+import models.Pet;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestOrder extends TestBase {
+    @Test
+    public void testAddNewOrder(){
+        // Add new pet to the store
+        Pet petToAdd = new Pet().generateTestData();
+        response = query.addPet(petToAdd);
+
+        Assert.assertEquals(200, response.getStatusCode());
+
+        // Add new order to the store
+        Order orderToAdd = new Order().generateTestData(petToAdd.getId());
+    }
+}
