a57b7f6fb1039f3db96939af817745a62dbce65d
Ludmila Haluchenko <duckmila@gmail.com>
2020-02-12 17:48:54 +0500

    TestPetStoreQuery - added addPet query method

--- src/test/java/tests/TestPet.java
+++ src/test/java/tests/TestPet.java
@@ -1,19 +1,23 @@
 package tests;
 
+import io.restassured.response.Response;
 import models.Pet;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestPet {
 
+    private static TestPetStoreQuery query;
+
     @BeforeClass
     public static void setTestConfiguration(){
-        TestPetStoreQuery query = new TestPetStoreQuery();
+        query = new TestPetStoreQuery();
         query.setQueryConfig("http://petstore.swagger/io");
     }
 
     @Test
     public void testAddNewPet(){
         Pet petToAdd = new Pet().generateTestData();
+        Response response = query.addPet(petToAdd);
     }
 }
--- src/test/java/tests/TestPetStoreQuery.java
+++ src/test/java/tests/TestPetStoreQuery.java
@@ -3,13 +3,29 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import models.Pet;
+
+import static io.restassured.RestAssured.given;
 
 public class TestPetStoreQuery {
+
     public void setQueryConfig (String websiteName){
         RestAssured.requestSpecification = new RequestSpecBuilder()
                 .setBaseUri(websiteName)
                 .setContentType(ContentType.JSON)
                 .addHeader("api_key", "duckmila")
                 .build();
+    }
+
+    public Response addPet(Pet petToAdd) {
+        return given()
+                .basePath("/v2/pet")
+                .body(petToAdd)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
     }
 }
